d8b0009a31a80a0f1c40cb6d488860bee049c447
Jorge Villalobos <JVS.Jorge@gmail.com>
2022-01-11 19:54:46 -0600

    second commit

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { pluck } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { Article, NewsResponse } from '../models/general.model';
+import { storedArticlesByCategory } from '../../assets/data/mock-news';
 
 const API_KEY = environment.newsApiKey;
+const storedArticlesByCategoryData = storedArticlesByCategory;
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,10 @@ const API_KEY = environment.newsApiKey;
 export class NewsService {
 
   public getNewsArticles(): Observable<Article[]> {
+    return of(storedArticlesByCategoryData.business.articles);
+  
+    // Next code is for real API call
+
     return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines`, {
       params: {
         country: 'us',
@@ -24,6 +30,9 @@ export class NewsService {
   }
 
   public getNewsArticlesByCategory(category: string): Observable<Article[]> {
+    return of(storedArticlesByCategoryData[category.toLowerCase()].articles);
+  
+    // Next code is for real API call
     return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines`, {
       params: {
         country: 'us',
